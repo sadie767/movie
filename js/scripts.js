fa0b70368c119c7e7cb167038b2210c672265fbf
javier jimenez and rebecca mcintosh <javier@>
2017-08-22 15:24:33 -0700

    add prototypes to our web movie page

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,22 @@
 //business logic
-function Ticket(movie, time, price) {
+function Ticket(movie, time, age) {
   this.movie = movie;
   this.time = time;
-  this.price = price;
+  this.age = age;
+  this.ticketPrice = 0;
 }
 
+Ticket.prototype.Age = function() {
+  if(this.age >=55 || this.age <= 12) {
+    return 5;
+  } else {
+    return 17;
+  };
+};
+
+Ticket.prototype.FinalPrice = function(){
+  return 0 + this.Age();
+}
 
 
 //user-interface logic
@@ -13,13 +25,13 @@ $(document).ready(function() {
     event.preventDefault();
     var pickMovie = $("#chooseMovie").val();
     var pickTime = $("#chooseTime").val();
-    var pickPrice = $("#choosePrice").val();
-    var newTicket = new Ticket(pickMovie, pickTime, pickPrice);
+    var userAge = $("#enterAge").val();
+    var newTicket = new Ticket(pickMovie, pickTime, userAge);
 
     $("#showResults").show();
     $("#showMovie").text(newTicket.movie);
     $("#showTime").text(newTicket.time);
-    $("#showPrice").text(newTicket.price);
+    $("#showPrice").text(newTicket.FinalPrice());
 
   });
 
